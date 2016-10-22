2cd01713ce5a4f3167f69ee21d65fbc56f48b963
Sven van Rijen <sven@svenvanrijen.nl>
2016-10-22 16:59:53 +0200

    Added Get-CPUAndBatteryInfo

--- CPUAndBatteryInfo.psm1
+++ CPUAndBatteryInfo.psm1
@@ -92,6 +92,10 @@ function Get-BatteryInfo
     
   Process
   {
+  
+    Write-Host -Object 'Caution: this command can only be used on a laptop!' -ForegroundColor Yellow
+    
+    Start-Sleep -Seconds 5
 
     $range = 1..$Repeats
     
@@ -106,6 +110,77 @@ function Get-BatteryInfo
            
         $properties = [ordered]@{
           'ComputerName'       = $Computer
+          'Battery Availability' = $Bat.Availability
+          'Battery Status'     = $Bat.Status
+          'Battery Active'     = $BatStat.Active
+          'Charge Rate'        = $BatStat.ChargeRate
+          'Charging?'          = $BatStat.Charging
+          'Power Online?'      = $BatStat.PowerOnline
+        }
+
+        $output = New-Object -TypeName PSObject -Property $properties
+        Write-Output -InputObject $output
+        Start-Sleep -Seconds $Wait                            
+      }
+    }
+    
+  }
+    
+  End
+  {
+  }
+}
+
+function Get-CPUAnBatteryInfo
+{
+  [OutputType([int])]
+  Param
+  (
+    # Param1 help description
+    [Parameter(ValueFromPipelineByPropertyName = $true,
+    Position = 0)]
+    [Alias('cn')]           
+    [string[]]$ComputerName = $env:COMPUTERNAME,
+
+    # Param2 help description
+    [Parameter(ValueFromPipelineByPropertyName = $true)]       
+    [int]$Repeats = '10',
+
+    # Param3 help description
+    [Parameter(ValueFromPipelineByPropertyName = $true)]       
+    [int]$Wait = '10'
+                  
+  )
+
+  Begin
+  {
+  }
+    
+  Process
+  {
+  
+    Write-Host -Object 'Caution: this command can only be used on a laptop!' -ForegroundColor Yellow
+    
+    Start-Sleep -Seconds 5
+
+    $range = 1..$Repeats
+    
+    foreach ($Computer in $ComputerName) 
+    {
+      Foreach ($i in $range) 
+      {
+        $namespace = 'root\WMI' 
+        Write-Verbose -Message "Connecting to $Computer"
+        $CPU = Get-WmiObject -ComputerName $Computer -Class Win32_Processor
+        $Bat = Get-WmiObject -ComputerName $Computer -Class Win32_Battery
+        $BatStat = Get-WmiObject -Class BatteryStatus -ComputerName $Computer -Namespace $namespace 
+           
+        $properties = [ordered]@{
+          'ComputerName'       = $Computer
+          'CPU Name'           = $CPU.Name
+          'Current Clock Speed' = $CPU.CurrentClockSpeed
+          'Maximum Clock Speed' = $CPU.MaxClockSpeed
+          'Load Percentage'    = $CPU.LoadPercentage
           'Battery Availability' = $Bat.Availability
           'Battery Status'     = $Bat.Status
           'Battery Active'     = $BatStat.Active
