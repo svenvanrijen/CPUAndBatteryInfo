7e8c93cce0bdc6469d12ab0e2ba14120ebd8f8ea
Sven van Rijen <RijenSHFC@rabobank.corp>
2016-10-22 16:36:21 +0200

    Added Get-BatteryInfo
    And some small changes

--- CPUAndBatteryInfo.psm1
+++ CPUAndBatteryInfo.psm1
@@ -17,7 +17,7 @@ function Get-CPUInfo
     [Parameter(ValueFromPipelineByPropertyName = $true,
     Position = 0)]
     [Alias('cn')]           
-    [string[]]$ComputerName = 'localhost',
+    [string[]]$ComputerName = $env:COMPUTERNAME,
 
     # Param2 help description
     [Parameter(ValueFromPipelineByPropertyName = $true)]       
@@ -52,6 +52,68 @@ function Get-CPUInfo
           'Maximum Clock Speed' = $CPU.MaxClockSpeed
           'Load Percentage'   = $CPU.LoadPercentage
         }
+        $output = New-Object -TypeName PSObject -Property $properties
+        Write-Output -InputObject $output
+        Start-Sleep -Seconds $Wait                            
+      }
+    }
+    
+  }
+    
+  End
+  {
+  }
+}
+
+function Get-BatteryInfo
+{
+  [OutputType([int])]
+  Param
+  (
+    # Param1 help description
+    [Parameter(ValueFromPipelineByPropertyName = $true,
+    Position = 0)]
+    [Alias('cn')]           
+    [string[]]$ComputerName = $env:COMPUTERNAME,
+
+    # Param2 help description
+    [Parameter(ValueFromPipelineByPropertyName = $true)]       
+    [int]$Repeats = '10',
+
+    # Param3 help description
+    [Parameter(ValueFromPipelineByPropertyName = $true)]       
+    [int]$Wait = '10'
+                  
+  )
+
+  Begin
+  {
+  }
+    
+  Process
+  {
+
+    $range = 1..$Repeats
+    
+    foreach ($Computer in $ComputerName) 
+    {
+      Foreach ($i in $range) 
+      {
+        $namespace = "root\WMI" 
+        Write-Verbose -Message "Connecting to $Computer"
+        $Bat = Get-WmiObject -ComputerName $Computer -Class Win32_Battery
+        $BatStat =  Get-WmiObject -class BatteryStatus -computername $Computer -namespace $namespace 
+           
+        $properties = [ordered]@{
+          'ComputerName'    = $Computer
+          'Battery Availability' = $Bat.Availability
+          'Battery Status' = $Bstatus
+          'Battery Active' = $BatStat.Active
+          'Charge Rate'= $BatStat.ChargeRate
+          'Charging?' = $BatStat.Charging
+          'Power Online?' = $BatStat.PowerOnline   
+        }
+
         $output = New-Object -TypeName PSObject -Property $properties
         Write-Output -InputObject $output
         Start-Sleep -Seconds $Wait                            
