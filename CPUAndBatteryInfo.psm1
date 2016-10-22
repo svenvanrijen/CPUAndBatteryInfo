4d4824d187ba8045054a0e7ab03f1f14a34a21bc
Sven van Rijen <sven@svenvanrijen.nl>
2016-10-22 15:29:23 +0200

    Wait and repeats added

--- CPUAndBatteryInfo.psm1
+++ CPUAndBatteryInfo.psm1
@@ -1,50 +1,66 @@
 ﻿<#
-.Synopsis
-   Short description
-.DESCRIPTION
-   Long description
-.EXAMPLE
-   Example of how to use this cmdlet
-.EXAMPLE
-   Another example of how to use this cmdlet
+    .Synopsis
+    Short description
+    .DESCRIPTION
+    Long description
+    .EXAMPLE
+    Example of how to use this cmdlet
+    .EXAMPLE
+    Another example of how to use this cmdlet
 #>
 function Get-CPUInfo
 {
-    [OutputType([int])]
-    Param
-    (
-        # Param1 help description
-        [Parameter(ValueFromPipelineByPropertyName=$true,
-                   Position=0)]
-        [Alias('cn')]           
-        [string[]]$ComputerName = 'localhost'
-                  
-    )
+  [OutputType([int])]
+  Param
+  (
+    # Param1 help description
+    [Parameter(ValueFromPipelineByPropertyName = $true,
+    Position = 0)]
+    [Alias('cn')]           
+    [string[]]$ComputerName = 'localhost',
 
-    Begin
-    {
-    }
+    # Param2 help description
+    [Parameter(ValueFromPipelineByPropertyName = $true)]       
+    [int]$Repeats = '10',
+
+    # Param3 help description
+    [Parameter(ValueFromPipelineByPropertyName = $true)]       
+    [int]$Wait = '10'
+                  
+  )
+
+  Begin
+  {
+  }
     
-    Process
-    {
+  Process
+  {
+
+    $range = 1..$Repeats
     
-  foreach ($Computer in $ComputerName)
+    foreach ($Computer in $ComputerName) 
     {
-      Write-Verbose "Connecting to $Computer"
-      $CPU = Get-WMIObject -ComputerName $Computer -Class Win32_Processor  
+      Foreach ($i in $range) 
+      {
+        Write-Verbose -Message "Connecting to $Computer"
+        $CPU = Get-WmiObject -ComputerName $Computer -Class Win32_Processor  
             
-      $properties = [ordered]@{'ComputerName'= $Computer;
-                      'CPU Name'   = $CPU.Name;
-                      'Current Clock Speed' = $CPU.CurrentClockSpeed;
-                      'Maximum Clock Speed' = $CPU.MaxClockSpeed;
-                      'Load Percentage' = $CPU.LoadPercentage}
-            $output = New-Object -TypeName PSObject -Property $properties
-            Write-Output $output
-     }
-    
-    
+        $properties = [ordered]@{
+          'ComputerName'      = $Computer
+          'CPU Name'          = $CPU.Name
+          'Current Clock Speed' = $CPU.CurrentClockSpeed
+          'Maximum Clock Speed' = $CPU.MaxClockSpeed
+          'Load Percentage'   = $CPU.LoadPercentage
+        }
+        $output = New-Object -TypeName PSObject -Property $properties
+        Write-Output -InputObject $output
+        Start-Sleep -Seconds $Wait                            
+      }
     }
-    End
-    {
-    }
+    
+  }
+    
+  End
+  {
+  }
 }
