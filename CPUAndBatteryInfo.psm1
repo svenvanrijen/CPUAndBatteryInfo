79050a0ade205c922c4d72612739782c024ef0e4
Sven van Rijen <RijenSHFC@rabobank.corp>
2016-10-24 22:38:20 +0200

    Updated .psm1 again

--- CPUAndBatteryInfo.psm1
+++ CPUAndBatteryInfo.psm1
@@ -1,29 +1,36 @@
 ﻿<#
     .Synopsis
-    Short description
+    This module generates diagnostic data, that can be used in case of slow boot and/or logon of your laptop.
     .DESCRIPTION
-    Long description
+    This module generates diagnostic data, that can be used in case of slow boot and/or logon of your laptop. 
+    Data on both CPU and Battery can be generated. Please note that generating battery data is only supported on laptops.
     .EXAMPLE
-    Example of how to use this cmdlet
+    Get-CPUInfo -Repeat 1 -Wait 1
+    In this example, you'll receive CPU info from the localhost (default). The script will just run once (default is 10). 
+    The timeout after every run is 1 second (default is 10).
     .EXAMPLE
-    Another example of how to use this cmdlet
+    Get-BatteryInfo -Computername client1 -Repeat 3 -Wait 5
+    In this example, you'll receive Battery info from client1 (default is localhost). The script will just run three times (default is 10). 
+    The timeout after every run is 5 seconds (default is 10).
+    .EXAMPLE
+    Get-CPUAndBatteryInfo -ComputerName client1, client2
+    In this example, you'll receive both CPU and Battery info, from both client1 and client2 (default is localhost only).
+    The script will run ten times (default). 
+    The timeout after every run is 10 seconds (default).
 #>
 function Get-CPUInfo
 {
   [OutputType([int])]
   Param
   (
-    # Param1 help description
     [Parameter(ValueFromPipelineByPropertyName = $true,
     Position = 0)]
     [Alias('cn')]           
     [string[]]$ComputerName = $env:COMPUTERNAME,
 
-    # Param2 help description
     [Parameter(ValueFromPipelineByPropertyName = $true)]       
     [int]$Repeats = '10',
 
-    # Param3 help description
     [Parameter(ValueFromPipelineByPropertyName = $true)]       
     [int]$Wait = '10'
                   
@@ -44,9 +51,8 @@ function Get-CPUInfo
       {
         Write-Verbose -Message "Connecting to $Computer"
         $CPU = Get-WmiObject -ComputerName $Computer -Class Win32_Processor  
-        $datetime = Get-Date    
+            
         $properties = [ordered]@{
-          'Date and time'      = $datetime
           'ComputerName'      = $Computer
           'CPU Name'          = $CPU.Name
           'Current Clock Speed' = $CPU.CurrentClockSpeed
@@ -104,14 +110,12 @@ function Get-BatteryInfo
     {
       Foreach ($i in $range) 
       {
-        $datetime = Get-Date
         $namespace = 'root\WMI' 
         Write-Verbose -Message "Connecting to $Computer"
         $Bat = Get-WmiObject -ComputerName $Computer -Class Win32_Battery
         $BatStat = Get-WmiObject -Class BatteryStatus -ComputerName $Computer -Namespace $namespace 
            
         $properties = [ordered]@{
-          'Date and time'      = $datetime
           'ComputerName'       = $Computer
           'Battery Availability' = $Bat.Availability
           'Battery Status'     = $Bat.Status
@@ -174,13 +178,11 @@ function Get-CPUAndBatteryInfo
       {
         $namespace = 'root\WMI' 
         Write-Verbose -Message "Connecting to $Computer"
-        $datetime = Get-Date
         $CPU = Get-WmiObject -ComputerName $Computer -Class Win32_Processor
         $Bat = Get-WmiObject -ComputerName $Computer -Class Win32_Battery
         $BatStat = Get-WmiObject -Class BatteryStatus -ComputerName $Computer -Namespace $namespace 
            
         $properties = [ordered]@{
-          'Date and time'      = $datetime
           'ComputerName'       = $Computer
           'CPU Name'           = $CPU.Name
           'Current Clock Speed' = $CPU.CurrentClockSpeed
